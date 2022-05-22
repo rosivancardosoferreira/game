406bdc99d34f0a12f05efd794efa5b8f9b18f28d
rosivancardoso <rosivan.ferreira@gok.digital>
2022-05-21 23:57:42 -0400

    chore: absolute path setting

---
+++ src/presentation/components/Button/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import {Button} from 'react-native';
+
+export function ComponentButton() {
+  return <Button title="Caminho" />;
+}
---
+++ src/presentation/components/screens/public/Login/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View} from 'react-native';
+
+import {ComponentButton} from 'presentation/components/Button';
+
+export function Login() {
+  return (
+    <View>
+      <ComponentButton />
+    </View>
+  );
+}
